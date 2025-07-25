a2b384e560cf8b1be2a47bb91354ceced3158618
Eelaf-Adam <e.adam@alustudent.com>
2025-07-25 16:25:01 +0200

    Error handling for auth

--- controllers/auth.py
+++ controllers/auth.py
@@ -1,3 +1,7 @@
+# This files contains the registeration process for the user of EcoWate App. It enables users to signup or login. 
+
+import re 
+
 class User:
     def __init__(self, email, password, role, **info):
         self.email = email
@@ -8,15 +12,31 @@ class User:
     def home(self):
         print(f"\n Welcome to the {self.role.capitalize()} Home Page!")
 
-
 class AuthSystem:
     def __init__(self):
         self.user = {}
 
+    def is_valid_email(self, email):  # Error handling for the email input 
+        # simple regex for validating user email 
+        pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+        return re.match(pattern, email) is not None
+    
+    def is_valid_password(self, password): #Error handling for the password input 
+        # 5 to 8 charcters, only letters and numbers
+        if not (5 <= len(password) <= 8):
+            return False
+        return password.isalnum()
+    
     def start(self):
         print("")
-        print("Welcome to the EcoWaste CLI App")
-        email = input("Enter your email: ").strip()
+        print("\n 🌱 Welcome to the EcoWaste CLI App")
+
+        while True: # Error handling 
+            email = input("Enter your email: ").strip().lower()
+            if not self.is_valid_email(email):
+                print("❌ Invalid email format. Please try again.")
+                continue
+            break
 
         if self.user_exsits(email):
             return self.login(email)
@@ -30,17 +50,26 @@ class AuthSystem:
 
     def login(self, email):
         user = self.user[email]
-        password = input("Enter your password: ").strip()
+        max_attempts = 3
 
-        if password == user.password:
-            print("Login successful")
-            user.home()
-            return user
+        for attempt in range(max_attempts):
+            password = input("Enter your password: ").strip()
+            if password == user.password:
+                print("🔐Login successful")
+                user.home()
+                return user
         else:
-            print("Incorrect password. Please try again.")
-            return None
+            attempts_left = max_attempts - attempt - 1
+            print(f" ❌ Incorrect password. Attempts left: {attempts_left}")
+            
+        print(" 🚫Too many failed attempts. Login aborted.")
+        return None
 
     def signup(self,email):
+        if self.user_exists(email):
+            print("⚠️ An account with this email already exsits.")
+            return None
+        
         print(" No account found. Let's create one.")
         print("")
         print("Are you signing up as:")
@@ -71,8 +100,23 @@ class AuthSystem:
             print("")
             name = input("Your Name: ").strip()
             location = input("Location: ").strip()
-            password = input("Create password: ").strip()
 
+        else:
+            print("Invalid Selection. Signup aborted.")
+            return None 
+        
+        # Password validation with confirmation 
+        while True:
+            password = input("Create password (5-8 chars, no symbols): ").strip()
+            if not elf.is_valid_password(password):
+                print("Inavlid password. Must be 5-8 letters/numbers only.")
+                continue
+            confirm = input("Confirm password:").strip()
+            if password != confirm:
+                print("Password do not match.")
+                continue
+            break
+      
 
             user = User(
                 email = email,
@@ -81,7 +125,7 @@ class AuthSystem:
                 name = name,
                 location = location
             )
-
+        
         else:
             print("Invalid selection. Signup aborted.")
             return None
