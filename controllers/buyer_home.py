bc7aca8e9fe1500f171012d0c122b066969c6208
L-nsamba <l.nsamba@alustudent.com>
2025-07-25 13:14:51 +0200

    Update 1.2

--- controllers/buyer_home.py
+++ controllers/buyer_home.py
@@ -6,7 +6,7 @@ class BuyerMenu:
         while True:
             print("\n Buyer Menu")
             print("1. Browse available waste")
-            print("2.View purchase history")
+            print("2. View purchase history")
             print("3. Logout")
 
             choice = input("Enter your choice: ").strip()
@@ -22,8 +22,55 @@ class BuyerMenu:
                 print("Invalid choice. Please try again.")
 
     def browse_waste(self):
-        print(" (Placeholder) Browsing waste...")
+        while True:
+            try:
+                print("Browsing waste...")
+                print("")
+                print("*** Waste Category ***")
+                print("1. Organic ")
+                print("2. Inorganic")
 
+                category_selection = int(input("Enter waste category [1-2]: "))
+
+                if category_selection == 1:
+                    print("Organic selected...")
+                    print("")
+                    print("*** Waste Types Avaliable ***")
+                    print("1. Food waste")
+                    print("2. Animal waste")
+                    print("3. Yard waste")
+
+                    type_selection = int(input("Enter waste type [1-3]: "))
+                    waste_types = {
+                        1: "Food waste",
+                        2: "Animal waste",
+                        3: "Yard waste"
+                    }
+
+                    if type_selection in waste_types:
+                        print("")
+                        print(f"{waste_types[type_selection]} selected...")
+
+                        qty = float(input("Enter quantity (in kg): "))
+                        location = input("Enter pickup location: ")
+
+                        return {
+                            "category": "Organic",
+                            "type": waste_types[type_selection],
+                            "quantity": qty,
+                            "location": location
+                        }
+                    else:
+                        print("Invalid waste type. Please try again.")
+
+
+                elif category_selection == 2:
+                    print("Inorganic selected...")
+                    break
+                else:
+                    print("Invalid numeric entry.")
+            except ValueError:
+                print("Invalid entry. Please try again!")
 
     def view_history(self):
-        print(" (Placeholder) Viewing purchase history...")
+        print("Viewing purchase history...")
